cac1ec150dea8da977e08c0e56be6bf73febd439
Wesley Moore <wes@wezm.net>
2017-02-05 21:11:23 +1100

    Make the App class own all the application components

--- lib/app.py
+++ lib/app.py
@@ -1,6 +1,21 @@
-from . import gtkui
+from . import conf, manager, service, XInterface, gtkui, app
 
 """Main application class"""
 class App:
     def __init__(self):
+        self._hotkeys_manager = manager.Hotkeys()
+        self._conf_manager = manager.Conf(self._hotkeys_manager)
+
+        self._phrases_manager = manager.Phrases(self._hotkeys_manager)
+        self._service = service.Service()
+        self._interface = XInterface.Interface()
+
+    def start(self):
+        self._hotkeys_manager.grab_hotkeys()
+        # Necessary to actually grab tab key, hopefully focus is not
+        # in a textbox during startup.
+        self._interface.send_string('\t')
+
+        self._service.start()
+        self._interface.start()
         gtkui.Indicator()
